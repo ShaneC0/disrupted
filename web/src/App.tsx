e78a30b7c01f1f3e1928a1382761c614d2cc4c8a
= <connors.shane@hotmail.com>
2021-07-30 12:31:02 -0400

    login logic on client side

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './styles/App.css';
 import Auth from './components/Auth'
 import Main from './components/Main'
+import axios from "axios"
+import API_URL from "./constants"
+
 
 export default function App() {
   let [isLoggedIn, setIsLoggedIn] = useState(false)
+  let [user, setUser] = useState({})
+
+  useEffect(() => {
+    if(localStorage.token) {
+      axios.get(`${API_URL}`)
+          .then(response => {
+            if(response.data.user) {
+              setUser(response.data.user)
+              setIsLoggedIn(true)
+            }
+          }).catch(error => {
+              console.error(error)
+          })
+    }
+  }, [])
 
   return (
     <div className="App">
